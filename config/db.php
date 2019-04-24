15632ea8d3045c9b9f0bba4df3d2a522e366a870
exp <5449554@gmail.com>
2019-04-24 23:56:45 +0300

    up

--- config/db.php
+++ config/db.php
@@ -1,14 +1,27 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+//    'username' => 'root',
+//    'password' => '',
+//    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
+
+
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=uteka',
+    'username' => 'postgres',
+    'password' => '',
+    'charset' => 'utf8',
+    'schemaMap' => [
+        'pgsql'=> [
+            'class'=>'yii\db\pgsql\Schema',
+            'defaultSchema' => 'public' //specify your schema here
+        ]
+    ], // PostgreSQL
 ];
